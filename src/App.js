1b04c47424de14960dcbd20e001fa4817fb53c0c
testgithub1912 <64775276+testgithub1912@users.noreply.github.com>
2020-09-04 10:16:10 +0530

    dasdasd

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React EASILY!
+          Learn React tesets!
         </a>
       </header>
     </div>

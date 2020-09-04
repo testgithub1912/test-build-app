867c9f2bdd55c6ea73b0b053ebec29a54e744feb
testgithub1912 <64775276+testgithub1912@users.noreply.github.com>
2020-09-04 11:21:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Titan
+          Learn React with Titan!
         </a>
       </header>
     </div>

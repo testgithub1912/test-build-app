1c3bc9603efbc8162b832ec99dbfa9f182cd6133
testgithub1912 <64775276+testgithub1912@users.noreply.github.com>
2020-09-30 17:54:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Titan!
+          Learn React with Lazygrid!
         </a>
       </header>
     </div>

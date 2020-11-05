63b1863aefd195068b63bf3d45aa4881d7faa9b4
testgithub1912 <64775276+testgithub1912@users.noreply.github.com>
2020-11-05 16:13:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Lazygrid!!
+          Learn React with Lazygrid in PRODUCTION!!
         </a>
       </header>
     </div>
